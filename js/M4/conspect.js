8f2d5d8610f4ed6d73ac5a820a585c4f28544d8c
Andriy Popovych <125279790+ampopovych@users.noreply.github.com>
2023-06-27 15:03:39 +0300

    Initial commit

---
+++ js/M4/conspect.js
@@ -0,0 +1,28 @@
+// function greet(name) {
+//   return `Ласкаво просимо ${name}.`;
+// }
+
+// Викликаємо функцію greet і виводимо результат у консоль
+// console.log(greet("Манго")); // Ласкаво просимо Манго.
+
+// Виводимо функцію greet у консоль, не викликаючи її
+// console.log(greet);
+
+
+
+
+function greet(name) {
+  console.log(`Ласкаво просимо ${name}.`);
+}
+
+// Функція вищого порядку
+function registerGuest(name, callback) {
+  console.log(`Реєструємо гостя ${name}.`);
+  callback(name);
+}
+
+registerGuest("Манго", greet);
+
+
+
+
